aef54d3c92a38071badf2c659e7f5397992fecf6
LaurieLecomte <lalec31@manitou.ibis.ulaval.ca>
2023-01-25 10:17:32 -0500

    corrected bam vs sample and output in srun command

--- 01_scripts/03_genotype.sh
+++ 01_scripts/03_genotype.sh
@@ -4,7 +4,7 @@
 # SV calling is done by sample for high-coverage genomes or in small batches for low-coverage genomes : we have high coverage (16X)
 # Following instructions for germline SV calling (https://github.com/dellytools/delly#germline-sv-calling)
 
-# parallel -a 02_infos/ind_ALL.txt -k -j 10 srun -c 1 -J 03_genotype_{} -log/03_genotype_{}_%j.log /bin/sh 01_scripts/03_genotype.sh {} &
+# parallel -a 02_infos/ind_ALL.txt -k -j 10 srun -c 1 -J 03_genotype_{} -o log/03_genotype_{}_%j.log /bin/sh 01_scripts/03_genotype.sh {} &
 
 # VARIABLES
 GENOME="03_genome/genome.fasta"
@@ -15,6 +15,7 @@ MERGED_DIR="06_merged"
 FILT_DIR="07_filtered"
 
 SAMPLE=$1
+BAM="$BAM_DIR/"SAMPLE".bam"
 
 REGIONS_EX="02_infos/excl_chrs.txt"
 
@@ -25,4 +26,4 @@ then
 fi
 
 # 1. Genotype this merged SV site list across all samples. This can be run in parallel for each sample.
-delly call -g $GENOME -v $CALLS_DIR/merged_sites.bcf -o $CALLS_DIR/geno/"$sample_ID".geno.bcf $SAMPLE
+delly call -g $GENOME -v $CALLS_DIR/merged_sites.bcf -o $CALLS_DIR/geno/"$sample_ID".geno.bcf $BAM
